e5ea5beeb6f3f878c5f5a6792fc73b404321946d
d4vyncy <davidserrudo@gmail.com>
2025-08-27 01:50:22 +0000

    clase 3

--- 03-BASES/app.js
+++ 03-BASES/app.js
@@ -2,7 +2,15 @@
 const { getcrearArchivo } = require('./helpers/multiplicar');
 
 console.clear();
-const base = 16;
+
+const [
+    , , arg3 = 'base=5'
+] = process.argv;
+const [, base] = arg3.split('=');
+
+console.log(base);
+
+// const base = 16;
 getcrearArchivo(base)
     .then(nombreArchivo => console.log(nombreArchivo, 'creado'))
     .catch(err => console.log(err));
